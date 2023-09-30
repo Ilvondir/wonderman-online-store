e509702ca15fdfbf08016ca5de5cdbe62bea4f55
Ilvondir <komsa.m@o2.pl>
2023-09-30 21:06:45 +0200

    gate created

--- wonderman-backend/app/Http/Controllers/ProductController.php
+++ wonderman-backend/app/Http/Controllers/ProductController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UpdateProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
--- wonderman-backend/app/Providers/AuthServiceProvider.php
+++ wonderman-backend/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Request;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        Gate::define("is_admin", function (User $user) {
+            return $user->role->name == "Admin";
+        });
     }
 }
