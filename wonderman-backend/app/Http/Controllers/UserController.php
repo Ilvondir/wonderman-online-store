f56e4ffba88dfc0f508b90e29effd5969e924831
Ilvondir <komsa.m@o2.pl>
2023-09-30 21:39:49 +0200

    change passwords added

--- wonderman-backend/app/Http/Controllers/UserController.php
+++ wonderman-backend/app/Http/Controllers/UserController.php
@@ -2,47 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChangePasswordRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Laravolt\Avatar\Avatar;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function createAdmin(RegisterRequest $request)
     {
-        //
+        $this->authorize("is_admin", $request->user());
+
+        $first_name = $request->validated(["first_name"]);
+        $last_name = $request->validated(["last_name"]);
+
+        if ($request->exists("avatar")) {
+
+            $filename = strtolower(Str::random(15)) . "." . $request->file("avatar")->extension();
+            $url = Storage::putFileAs("public/img/avatars", $request->validated("avatar"), $filename);
+
+        } else {
+
+            $filename = strtolower(Str::random(15)) . ".png";
+            $generator = new Avatar();
+            $file = $generator->create($first_name . " " . $last_name)->setBackground("#7f00ff")->toBase64();
+            $url = Storage::putFileAs("public/img/avatars", $file, $filename);
+
+        }
+
+        $user = User::create([
+            "first_name" => $first_name,
+            "last_name" => $last_name,
+            "email" => $request->validated(["email"]),
+            "login" => $request->validated(["login"]),
+            "password" => Hash::make($request->validated(["password"])),
+            "created" => date("Y-m-d"),
+            "avatar" => $url,
+            "role_id" => 1
+        ]);
+
+        return response(new UserResource($user->load("role")), Response::HTTP_CREATED);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function changePassword(ChangePasswordRequest $request)
     {
-        //
-    }
+        $user = $request->user();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
+        if (Hash::check($request->validated("old_password"), $user->password)) {
+            $user->update(["password" => Hash::make($request->validated("new_password"))]);
+            return response(["message" => "success"], Response::HTTP_ACCEPTED);
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response(["error" => "Old password is incorrect."], Response::HTTP_FORBIDDEN);
     }
 }
--- wonderman-backend/routes/api.php
+++ wonderman-backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,7 +27,9 @@ Route::get("/products/{id}", [ProductController::class, "show"]);
 
 Route::middleware("auth:sanctum")->group(function () {
     Route::get("/auth/user", [AuthController::class, "user"]);
+    Route::post("/auth/admin", [UserController::class, "createAdmin"]);
     Route::delete("/auth/logout", [AuthController::class, "logout"]);
+    Route::put("/auth/password", [UserController::class, "changePassword"]);
 
     Route::post("/products", [ProductController::class, "store"]);
     Route::put("/products/{id}", [ProductController::class, "update"]);
