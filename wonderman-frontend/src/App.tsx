ef21c4baeaabfe0506a868f45d0d2ea20422aa30
Ilvondir <komsa.m@o2.pl>
2023-10-07 16:57:56 +0200

    work started

--- wonderman-frontend/src/App.tsx
+++ wonderman-frontend/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Routes, BrowserRouter, Route, Navigate} from 'react-router-dom';
+import NotFound from "./pages/problems/404/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Navigate to={"/home"}/>}/>
+                <Route path={"/home"}/>
+
+                <Route path={"*"} element={<NotFound/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
