312632eea7fcdc91a72234e06219eaec64cd3411
Ilvondir <komsa.m@o2.pl>
2023-10-11 16:18:23 +0200

    footer created and work with scss started

--- wonderman-frontend/src/App.tsx
+++ wonderman-frontend/src/App.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import './App.css';
 import {Routes, BrowserRouter, Route, Navigate} from 'react-router-dom';
 import NotFound from "./pages/problems/404/NotFound";
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<Navigate to={"/home"}/>}/>
-                <Route path={"/home"}/>
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={<Navigate to={"/home"}/>}/>
+                    <Route path={"/home"}/>
 
-                <Route path={"*"} element={<NotFound/>}/>
-            </Routes>
-        </BrowserRouter>
+                    <Route path={"*"} element={<NotFound/>}/>
+                </Routes>
+            </BrowserRouter>
+
+            <Footer/>
+        </>
     );
 }
 
---
+++ wonderman-frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPhone, faEnvelope, faMap} from '@fortawesome/free-solid-svg-icons';
+
+
+const Footer = () => {
+    return (
+        <div className="footer col-12 padding-3">
+            <div className="row">
+                <div className="col-6">
+                    <div className="col-3 text-align-right">
+                        <div className="font-size-2 margin-bottom-2 margin-right-1"><FontAwesomeIcon icon={faMap}/>
+                        </div>
+                        <div className="font-size-2 margin-bottom-2 margin-right-1"><FontAwesomeIcon icon={faPhone}/>
+                        </div>
+                        <div className="font-size-2 margin-right-1"><FontAwesomeIcon icon={faEnvelope}/>
+                        </div>
+                    </div>
+                    <div className="col-9">
+                        <div className="font-size-2 margin-bottom-2"><a href="tel:+48539023985">+48 539 023 985</a>
+                        </div>
+                        <div className="font-size-2 margin-bottom-2"><a
+                            href="mailto:contact@wonderman.com">contact@wonderman.com</a></div>
+                        <div className="font-size-2">55 Shopping Street, <strong>London</strong></div>
+                    </div>
+                </div>
+                <div className="col-6 text-align-right padding-bottom-1 padding-left-1 padding-right-1  font-size-1">
+                    <h1 className="margin-top-0">Wonderman Store</h1>
+                    <p>
+                        Our online store offers a wide range of products to cater to every need and preference. Discover
+                        a diverse selection of high-quality items, from the latest fashion trends to cutting-edge
+                        electronics. Browse through our user-friendly interface, explore various categories, and find
+                        the perfect products at competitive prices. Enjoy a seamless shopping experience with secure
+                        payments.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
