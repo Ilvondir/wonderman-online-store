baa3c983cbf3c8d04511af06c5c633cf3b3abd72
Ilvondir <komsa.m@o2.pl>
2023-10-17 19:15:56 +0200

    backend bugfixes

--- wonderman-backend/app/Http/Controllers/ProductController.php
+++ wonderman-backend/app/Http/Controllers/ProductController.php
@@ -31,7 +31,8 @@ class ProductController extends Controller
                 "price",
                 "products.description AS description",
                 "photo",
-                "added"
+                "added",
+                "tax"
             ])
             ->orderByDesc("products.id")
             ->where("categories.name", "=", $category)
@@ -51,7 +52,7 @@ class ProductController extends Controller
 
         $products = Product::query()
             ->join("transactions", "transactions.product_id", "=", "products.id")
-            ->selectRaw("name, products.price AS price, description, added, photo, COUNT(transactions.id) AS freq")
+            ->selectRaw("name, products.price AS price, description, added, photo, COUNT(transactions.id) AS freq, tax")
             ->groupBy("transactions.product_id")
             ->orderByDesc("freq")
             ->paginate(12);
