fd29e02e604a3adf609bcaa009189bcfb9176b4d
Ilvondir <komsa.m@o2.pl>
2023-10-19 15:39:21 +0200

    purchase page created

--- wonderman-frontend/src/pages/purchase/PurchasePage.tsx
+++ wonderman-frontend/src/pages/purchase/PurchasePage.tsx
@@ -1,13 +1,82 @@
-import React from 'react';
-import {useParams} from "react-router-dom";
+import React, {SyntheticEvent, useEffect, useState} from 'react';
+import {Link, useParams} from "react-router-dom";
 import Wrapper from "../../components/Wrapper/Wrapper";
+import Spinner from "../../components/Spinner/Spinner";
+import axios from "axios";
+import {headers} from "../../axios/commons";
+import {Product} from "../../models/Product";
 
 const PurchasePage = () => {
     const {id} = useParams();
+    const [wait, setWait] = useState(true);
+    const [product, setProduct] = useState(new Product());
+    const [info, setInfo] = useState("");
+    const [clicked, setClicked] = useState(false);
+
+    useEffect(() => {
+        axios.get("/products/" + id, {headers: headers()})
+            .then(response => {
+                setWait(false);
+                setProduct(response.data);
+            })
+    }, []);
+
+    const purchase = (e: SyntheticEvent, id: any) => {
+        e.preventDefault();
+
+        setClicked(true);
+
+        axios.post("/products/" + product.id, {}, {headers: headers()})
+            .then(response => {
+                setClicked(false);
+                setInfo("Transaction created successfully.")
+            })
+            .catch(error => {
+                setClicked(false);
+                setInfo(error.response.data.message);
+            })
+
+    }
 
     return (
         <Wrapper>
-            {id}
+
+            <Spinner show={wait} customStyle={false}/>
+
+            <div className={wait ? "purchase-page hide" : "purchase-page"}>
+                <h2 style={{marginTop: 0}}>Purchase</h2>
+
+                <p>Do you want purchase a product <strong>{product.name}</strong>?</p>
+
+                <table className="text-align-center">
+                    <thead>
+                    <tr>
+                        <th>Netto</th>
+                        <th>Tax</th>
+                        <th>Brutto</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>{Number(product.netto).toFixed(2)} $</td>
+                        <td>{product.tax}%</td>
+                        <td><strong>{Number(product.brutto).toFixed(2)} $</strong></td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <Spinner show={clicked} customStyle={false}/>
+
+                <div className={clicked ? "text-align-center hide" : "text-align-center"}>
+                    <button onClick={(e) => purchase(e, product.id)}>Pay {Number(product.brutto).toFixed(2)} $</button>
+
+                    {info}
+
+                    <Link to={"/products/" + id}>
+                        <button>No, go back</button>
+                    </Link>
+                </div>
+            </div>
         </Wrapper>
     );
 };
